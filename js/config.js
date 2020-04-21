9c72bcda78c5903b67c0e87c9a0e3335e6c619d2
E-compfast Digital Indonesia <58408252+e-compfast@users.noreply.github.com>
2020-04-21 07:39:06 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -78,7 +78,7 @@ $(function() {
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
 
-		// Google Spreadsheet ID //https://docs.google.com/spreadsheets/d/1fY9rigZE8JawBWU8RMyzd7wOnrgCROKpQE5UvKONLxU/edit?usp=drivesdk
+		// Google Spreadsheet ID
 		spreadsheetID : "1fY9rigZE8JawBWU8RMyzd7wOnrgCROKpQE5UvKONLxU"
 
 	});
